da25a4ab0eb295feb7e44ee3f7f881aad1f2d487
Jayden Edara <aedara@uw.edu>
2023-08-14 18:06:30 -0700

    Fix naming issue in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.readlines()
 
 setup(
-    name='neon_search_engine',
+    name='neon-search-engine',
     version='0.5',
     description='Search Engines Scraper',
     author='Tasos M. Adamopoulos, Jayden E.',
